d0a67bff30a1e60c67c7362d21ad30fb47e4eb6f
Lukas <lukasschnittcher@web.de>
2020-11-25 15:51:45 +0100

    Relu Activation

--- Neural_Network/neural_network.jl
+++ Neural_Network/neural_network.jl
@@ -9,9 +9,21 @@ function sigmoid_derivation(x)
     return sigmoid(x)*(1-sigmoid(x))
 end
 
+function relu(x)
+    return max(0,x)
+end
+
+function relu_derivation(x)
+    if x < 0
+        return 0
+    elseif x > 0
+        return 1
+    end
+end
+
 function feed_forward(input, weights_input, weights_output)
     hidden_input = weights_input * input
-    hidden_out = map(x -> sigmoid(x), hidden_input)
+    hidden_out = map(x -> relu(x), hidden_input)
 
     final_input = weights_output * hidden_out
     final_out = map(x -> sigmoid(x), final_input)
@@ -19,15 +31,16 @@ function feed_forward(input, weights_input, weights_output)
     return hidden_out, final_out
 end
 
-function train(data, weights_input, weights_output, alpha)
+function train(data, weights_input, weights_output, alpha, activation, derivation)
     for i = 1:size(data)[1]
         input = data[i,:][1:19]
         hidden_out, final_out = feed_forward(input, weights_input, weights_output)
-        if(data[i,:][20] == 1)
-            target_out = [0, 1]
-        else
-            target_out = [1, 0]
-        end
+        #if(data[i,:][20] == 1)
+        #    target_out = [0, 1]
+        #else
+        #    target_out = [1, 0]
+        #end
+        target_out = [Int(data[i,20])]
 
         error_out = target_out - final_out
         error_hidden = transpose(weights_output)*error_out
@@ -35,7 +48,7 @@ function train(data, weights_input, weights_output, alpha)
         delta_out = error_out .* map(x -> sigmoid_derivation(x), final_out)
         weights_output = weights_output + learningrate*(delta_out*transpose(hidden_out))
 
-        delta_hidden = error_hidden .* map(x -> sigmoid_derivation(x), hidden_out)
+        delta_hidden = error_hidden .* map(x -> relu_derivation(x), hidden_out)
         weights_input = weights_input + learningrate*(delta_hidden*transpose(input))
 
     end
@@ -45,18 +58,17 @@ end
 function query(input, class, wih, who)
     hidden_out, final_out = feed_forward(input, wih, who)
     println("class is ", Int(class))
-    println("Accuracy for class 0 is ", final_out[1])
-    println("Accuracy for class 1 is ", final_out[2])
+    println("Accuracy is ", final_out[1])
+    #println("Accuracy for class 1 is ", final_out[2])
 end
 
-function predict_probability(input, weights_ih, weights_ho)
-    hidden_out, final_out = feed_forward(input, weights_ih, weights_ho)
+function predict_probability(input)
+    hidden_out, final_out = feed_forward(input, nwhi, nwho)
     return final_out
 end
 
-
 inputnodes = 19
-outputnodes = 2
+outputnodes = 1
 hiddennodes = 100
 learningrate = 0.05
 
@@ -72,8 +84,9 @@ nwhi =  weights_ih
 nwho = weights_ho
 
 for epochs = 1:6
-    global nwho, nwhi = train(norm_data[1:864,:], nwhi, nwho, learningrate)
+    global nwho, nwhi = train(norm_data[1:865,:], nwhi, nwho, learningrate)
 end
 
 #query(norm_data[1150,:][1:19], norm_data[1150,20], nwhi, nwho)
 #query(norm_data[1151,:][1:19], norm_data[1151,20], nwhi, nwho)
+println(predict_probability(norm_data[1150,:][1:19]))
