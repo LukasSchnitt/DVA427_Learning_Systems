1c018714fc7aedf14269fafb3aa90a9a90cb255d
Lukas <lukasschnittcher@web.de>
2020-11-25 15:22:28 +0100

    Add Testing

--- Neural_Network/neural_network.jl
+++ Neural_Network/neural_network.jl
@@ -3,7 +3,7 @@ include("FileInput.jl")
 
 inputnodes = 19
 outputnodes = 2
-hiddennodes = 100
+hiddennodes = 110
 learningrate = 0.1
 
 #Outputnodes = First Node is Class 0, Second Node is Class 1
@@ -34,36 +34,43 @@ function train(data, weights_input, weights_output, alpha)
     for i = 1:size(data)[1]
         input = data[i,:][1:19]
         hidden_out, final_out = feed_forward(input, weights_input, weights_output)
-        if(data[i,20] == 0)
-            target_out = [1,0]
+        if(data[i,:][20] == 1)
+            target_out = [0, 1]
         else
-            target_out = [0,1]
+            target_out = [1, 0]
         end
+
         error_out = target_out - final_out
-        delta_out = map(x -> sigmoid_derivation(x), final_out).*error_out
+        error_hidden = transpose(weights_output)*error_out
 
-        weights_output = weights_output + (-alpha*(delta_out*transpose(hidden_out)))
+        delta_out = error_out .* map(x -> sigmoid_derivation(x), final_out)
+        weights_output = weights_output + learningrate*(delta_out*transpose(hidden_out))
 
-        error_hidden = reshape(delta_out'weights_output,100,1)
-        delta_hidden = map(x -> sigmoid_derivation(x), hidden_out).*error_hidden
+        delta_hidden = error_hidden .* map(x -> sigmoid_derivation(x), hidden_out)
+        weights_input = weights_input + learningrate*(delta_hidden*transpose(input))
 
-        weights_input = weights_input + (-learningrate*(delta_hidden*transpose(input)))
     end
     return weights_output, weights_input
 end
 
 function query(input, class, wih, who)
     hidden_out, final_out = feed_forward(input, wih, who)
-    print("class is 0\n")
-    print("Accurancy for class 0 is ", final_out[1])
-    print("Accurancy for class 1 is ", final_out[2])
+    println("class is ", Int(class))
+    println("Accurancy for class 0 is ", final_out[1])
+    println("Accurancy for class 1 is ", final_out[2])
 end
 
 data = readdlm("Diabetic.txt", ',', Float32, '\n', skipstart = 24)
 norm_data = normalization(copy(data))
 
-query(norm_data[1,:], norm_data[1,20], weights_ih, weights_ho)
+nwhi =  weights_ih
+nwho = weights_ho
 
-nwho, nwhi = train(norm_data, weights_ih, weights_ho, learningrate)
+#query(norm_data[2,:][1:19], norm_data[2,20], weights_ih, weights_ho)
+println(size(norm_data))
+for i = 1:6
+    global nwho, nwhi = train(norm_data[1:864,:], nwhi, nwho, learningrate)
+end
 
-query(norm_data[1,:], norm_data[1,20], nwhi, nwho)
+query(norm_data[1150,:][1:19], norm_data[1150,20], nwhi, nwho)
+query(norm_data[1151,:][1:19], norm_data[1151,20], nwhi, nwho)
